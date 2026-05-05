f76e19b8803d80b5ee8da27702e1940fcf7a901c
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2026-05-05 14:04:10 +0200

    Add doc reference to testbed_CB_JTAG_probe

--- docs/run_sphinx_upload.py
+++ docs/run_sphinx_upload.py
@@ -48,6 +48,8 @@ REPOS: list[Repo] = [
     Repo("testbed_heatguard", entry="/introduction/index"),
     Repo("testbed_showcase", automodule=True, entry="/introduction/index"),
     Repo("testbed_micropython", entry="/user_guide/index"),
+    Repo("testbed_cb_jtag_probe", entry="/introduction/index"),
+    # Repo("testbed_zephyr", entry="/user_guide/index"),
 ]
 
 BASE_DIR = Path.home() / "work_octoprobe"
@@ -66,7 +68,7 @@ _INDEX_TOP_TEMPLATE = jinja2.Template("""
 .. Restructured text comment: IGNORE_SECTION_CHECK
 
 {{ repo.name }}
-====================
+====================================
 
 {% if repo.automodule -%}
 .. automodule:: {{ repo.directory }}
